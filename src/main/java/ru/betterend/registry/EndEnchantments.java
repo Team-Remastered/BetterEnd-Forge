aaa400db33d45fbd9e4bfe7d14abe4ab75236ee3
Jack <ilysalhi22@gmail.com>
2023-11-21 16:44:23 -0500

    Ported enchantment registry

--- src/main/java/ru/betterend/registry/EndEnchantments.java
+++ src/main/java/ru/betterend/registry/EndEnchantments.java
@@ -2,19 +2,25 @@ package ru.betterend.registry;
 
 import net.minecraft.core.Registry;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import ru.betterend.BetterEndForge;
 import ru.betterend.config.Configs;
 import ru.betterend.effects.enchantment.EndVeilEnchantment;
 
 public class EndEnchantments {
-	public final static Enchantment END_VEIL = registerEnchantment("end_veil", new EndVeilEnchantment());
-	
-	public static Enchantment registerEnchantment(String name, Enchantment enchantment) {
-		if (!Configs.ENCHANTMENT_CONFIG.getBooleanRoot(name, true)) {
-			return enchantment;
+
+	public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, BetterEndForge.MOD_ID);
+
+	public final static RegistryObject<Enchantment> END_VEIL = registerEnchantment("end_veil", new EndVeilEnchantment());
+
+		public static RegistryObject<Enchantment> registerEnchantment(String name, Enchantment enchantment) {
+			if (Configs.ENCHANTMENT_CONFIG.getBooleanRoot(name, true)) {
+				Registry.register(Registry.ENCHANTMENT, BetterEndForge.makeID(name), enchantment);
+			}
+			return ENCHANTMENTS.register(name, () -> enchantment);
 		}
-		return Registry.register(Registry.ENCHANTMENT, BetterEndForge.makeID(name), enchantment);
-	}
 	
 	public static void register() {}
 }
