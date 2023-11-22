fe96a43f35d712dbadfe58aaf81201d0296e30cd
Jack <ilysalhi22@gmail.com>
2023-11-21 20:40:48 -0500

    Ported particle registry

--- src/main/java/ru/betterend/registry/EndParticles.java
+++ src/main/java/ru/betterend/registry/EndParticles.java
@@ -1,11 +1,17 @@
 package ru.betterend.registry;
 
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.core.Registry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import ru.betterend.BetterEndForge;
 import ru.betterend.particle.FireflyParticle;
 import ru.betterend.particle.InfusionParticle;
@@ -20,47 +26,61 @@ import ru.betterend.particle.ParticleTenaneaPetal;
 import ru.betterend.particle.PaticlePortalSphere;
 import ru.betterend.particle.SmaragdantParticle;
 
+@Mod.EventBusSubscriber(modid = BetterEndForge.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EndParticles {
-	public static final SimpleParticleType GLOWING_SPHERE = register("glowing_sphere");
-	public static final SimpleParticleType PORTAL_SPHERE = register("portal_sphere");
-	public static final ParticleType<InfusionParticleType> INFUSION = register(
-		"infusion",
-		FabricParticleTypes.complex(InfusionParticleType.PARAMETERS_FACTORY)
-	);
-	public static final SimpleParticleType SULPHUR_PARTICLE = register("sulphur_particle");
-	public static final SimpleParticleType GEYSER_PARTICLE = registerFar("geyser_particle");
-	public static final SimpleParticleType SNOWFLAKE = register("snowflake");
-	public static final SimpleParticleType AMBER_SPHERE = register("amber_sphere");
-	public static final SimpleParticleType BLACK_SPORE = register("black_spore");
-	public static final SimpleParticleType TENANEA_PETAL = register("tenanea_petal");
-	public static final SimpleParticleType JUNGLE_SPORE = register("jungle_spore");
-	public static final SimpleParticleType FIREFLY = register("firefly");
-	public static final SimpleParticleType SMARAGDANT = register("smaragdant_particle");
+	public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, BetterEndForge.MOD_ID);
+
+	public static void register(IEventBus eventBus) {
+		PARTICLE_TYPES.register(eventBus);
+	}
+
+	public static final RegistryObject<SimpleParticleType> GLOWING_SPHERE = register("glowing_sphere");
+	public static final RegistryObject<SimpleParticleType> PORTAL_SPHERE = register("portal_sphere");
+	public static final RegistryObject<ParticleType<InfusionParticleType>> INFUSION = register("infusion", complex(InfusionParticleType.PARAMETERS_FACTORY));
+	public static final RegistryObject<SimpleParticleType> SULPHUR_PARTICLE = register("sulphur_particle");
+	public static final RegistryObject<SimpleParticleType> GEYSER_PARTICLE = registerFar("geyser_particle");
+	public static final RegistryObject<SimpleParticleType> SNOWFLAKE = register("snowflake");
+	public static final RegistryObject<SimpleParticleType> AMBER_SPHERE = register("amber_sphere");
+	public static final RegistryObject<SimpleParticleType> BLACK_SPORE = register("black_spore");
+	public static final RegistryObject<SimpleParticleType> TENANEA_PETAL = register("tenanea_petal");
+	public static final RegistryObject<SimpleParticleType> JUNGLE_SPORE = register("jungle_spore");
+	public static final RegistryObject<SimpleParticleType> FIREFLY = register("firefly");
+	public static final RegistryObject<SimpleParticleType> SMARAGDANT = register("smaragdant_particle");
+
+	@SubscribeEvent
+	public static void registerParticuleFactory(ParticleFactoryRegisterEvent event) {
+		particleEngine(GLOWING_SPHERE.get(), ParticleGlowingSphere.FactoryGlowingSphere::new);
+		particleEngine(PORTAL_SPHERE.get(), PaticlePortalSphere.FactoryPortalSphere::new);
+		particleEngine(INFUSION.get(), InfusionParticle.InfusionFactory::new);
+		particleEngine(SULPHUR_PARTICLE.get(), ParticleSulphur.FactorySulphur::new);
+		particleEngine(GEYSER_PARTICLE.get(), ParticleGeyser.FactoryGeyser::new);
+		particleEngine(SNOWFLAKE.get(), ParticleSnowflake.FactorySnowflake::new);
+		particleEngine(AMBER_SPHERE.get(), ParticleGlowingSphere.FactoryGlowingSphere::new);
+		particleEngine(BLACK_SPORE.get(), ParticleBlackSpore.FactoryBlackSpore::new);
+		particleEngine(TENANEA_PETAL.get(), ParticleTenaneaPetal.FactoryTenaneaPetal::new);
+		particleEngine(JUNGLE_SPORE.get(), ParticleJungleSpore.FactoryJungleSpore::new);
+		particleEngine(FIREFLY.get(), FireflyParticle.FireflyParticleFactory::new);
+		particleEngine(SMARAGDANT.get(), SmaragdantParticle.SmaragdantParticleFactory::new);
+	}
+
+	private static RegistryObject<SimpleParticleType> register(String name) {
+		return PARTICLE_TYPES.register(name, () -> new SimpleParticleType(false));
+	}
+
 	
-	public static void register() {
-		ParticleFactoryRegistry.getInstance().register(GLOWING_SPHERE, ParticleGlowingSphere.FactoryGlowingSphere::new);
-		ParticleFactoryRegistry.getInstance().register(PORTAL_SPHERE, PaticlePortalSphere.FactoryPortalSphere::new);
-		ParticleFactoryRegistry.getInstance().register(INFUSION, InfusionParticle.InfusionFactory::new);
-		ParticleFactoryRegistry.getInstance().register(SULPHUR_PARTICLE, ParticleSulphur.FactorySulphur::new);
-		ParticleFactoryRegistry.getInstance().register(GEYSER_PARTICLE, ParticleGeyser.FactoryGeyser::new);
-		ParticleFactoryRegistry.getInstance().register(SNOWFLAKE, ParticleSnowflake.FactorySnowflake::new);
-		ParticleFactoryRegistry.getInstance().register(AMBER_SPHERE, ParticleGlowingSphere.FactoryGlowingSphere::new);
-		ParticleFactoryRegistry.getInstance().register(BLACK_SPORE, ParticleBlackSpore.FactoryBlackSpore::new);
-		ParticleFactoryRegistry.getInstance().register(TENANEA_PETAL, ParticleTenaneaPetal.FactoryTenaneaPetal::new);
-		ParticleFactoryRegistry.getInstance().register(JUNGLE_SPORE, ParticleJungleSpore.FactoryJungleSpore::new);
-		ParticleFactoryRegistry.getInstance().register(FIREFLY, FireflyParticle.FireflyParticleFactory::new);
-		ParticleFactoryRegistry.getInstance().register(SMARAGDANT, SmaragdantParticle.SmaragdantParticleFactory::new);
+	private static RegistryObject<SimpleParticleType> registerFar(String name) {
+		return PARTICLE_TYPES.register(name, () -> new SimpleParticleType(true));
 	}
 	
-	private static SimpleParticleType register(String name) {
-		return Registry.register(Registry.PARTICLE_TYPE, BetterEndForge.makeID(name), FabricParticleTypes.simple());
+	private static <T extends ParticleOptions> RegistryObject<ParticleType<T>> register(String name, ParticleType<T> type) {
+		return PARTICLE_TYPES.register(name, () -> type);
 	}
-	
-	private static SimpleParticleType registerFar(String name) {
-		return Registry.register(Registry.PARTICLE_TYPE, BetterEndForge.makeID(name), FabricParticleTypes.simple(true));
+
+	private static <T extends ParticleOptions> void particleEngine(ParticleType<T> particleType, ParticleEngine.SpriteParticleRegistration<T> particleRegistration) {
+		Minecraft.getInstance().particleEngine.register(particleType, particleRegistration);
 	}
-	
-	private static <T extends ParticleOptions> ParticleType<T> register(String name, ParticleType<T> type) {
-		return Registry.register(Registry.PARTICLE_TYPE, BetterEndForge.makeID(name), type);
+
+	public static <T extends ParticleOptions> ParticleType<T> complex(ParticleOptions.Deserializer<T> factory) {
+		return complex(factory);
 	}
 }
