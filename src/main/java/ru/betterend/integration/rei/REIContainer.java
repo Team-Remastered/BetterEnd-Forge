b653866ef47948913a2a0d969fc5da9ad38f134a
Jack <ilysalhi22@gmail.com>
2023-11-23 19:37:31 -0500

    Commented REI integration

--- src/main/java/ru/betterend/integration/rei/REIContainer.java
+++ src/main/java/ru/betterend/integration/rei/REIContainer.java
@@ -1,9 +1,9 @@
-package ru.betterend.integration.rei;
-
-public class REIContainer implements Runnable {
-	
-	@Override
-	public void run() {
-		//ContainerInfoHandler.registerContainerInfo(AlloyingRecipe.ID, CraftingContainerInfoWrapper.create(EndStoneSmelterScreenHandler.class));
-	}
-}
+//package ru.betterend.integration.rei;
+//
+//public class REIContainer implements Runnable {
+//
+//	@Override
+//	public void run() {
+//		//ContainerInfoHandler.registerContainerInfo(AlloyingRecipe.ID, CraftingContainerInfoWrapper.create(EndStoneSmelterScreenHandler.class));
+//	}
+//}
