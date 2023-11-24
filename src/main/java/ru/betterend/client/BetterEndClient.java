87c246a5b2df1075817263568d02a85db76b0667
Jack <ilysalhi22@gmail.com>
2023-11-23 21:07:43 -0500

    Moved BetterEnd Client entry to the main class

--- src/main/java/ru/betterend/client/BetterEndClient.java
+++ src/main/java/ru/betterend/client/BetterEndClient.java
@@ -1,35 +1,18 @@
 package ru.betterend.client;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Style;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
 import ru.betterend.bclib.BCLib;
 import ru.betterend.bclib.util.TranslationHelper;
 import ru.betterend.BetterEndForge;
-import ru.betterend.client.render.BetterEndSkyRenderer;
-import ru.betterend.events.ItemTooltipCallback;
 import ru.betterend.interfaces.MultiModelItem;
-import ru.betterend.item.CrystaliteArmor;
-import ru.betterend.registry.EndBlockEntityRenders;
-import ru.betterend.registry.EndEntitiesRenders;
 import ru.betterend.registry.EndModelProviders;
-import ru.betterend.registry.EndParticles;
-import ru.betterend.registry.EndScreens;
-import ru.betterend.world.generator.GeneratorOptions;
 
 public class BetterEndClient {
-	@Override
-	public void onInitializeClient() {
+
+	public static void initializeClient() {
 		EndModelProviders.register();
 		MultiModelItem.register();
 		ClientOptions.init();
-		registerTooltips();
 		
 		if (BCLib.isDevEnvironment()) {
 			TranslationHelper.printMissingEnNames(BetterEndForge.MOD_ID);
@@ -40,38 +23,41 @@ public class BetterEndClient {
 		ResourceLocation checkPlantId = new ResourceLocation("item/chorus_plant");
 		ResourceLocation toLoadFlowerId = new ResourceLocation("betterend", "item/custom_chorus_flower");
 		ResourceLocation toLoadPlantId = new ResourceLocation("betterend", "item/custom_chorus_plant");
-		ModelLoadingRegistry.INSTANCE.registerResourceProvider(manager -> (resourceId, context) -> {
-			if (GeneratorOptions.changeChorusPlant()) {
-				if (resourceId.equals(checkFlowerId)) {
-					return context.loadModel(toLoadFlowerId);
-				}
-				else if (resourceId.equals(checkPlantId)) {
-					return context.loadModel(toLoadPlantId);
-				}
-			}
-			return null;
-		});
+
+//		ModelLoadingRegistry.INSTANCE.registerResourceProvider(manager -> (resourceId, context) -> {
+//			if (GeneratorOptions.changeChorusPlant()) {
+//				if (resourceId.equals(checkFlowerId)) {
+//					return context.loadModel(toLoadFlowerId);
+//				}
+//				else if (resourceId.equals(checkPlantId)) {
+//					return context.loadModel(toLoadPlantId);
+//				}
+//			}
+//			return null;
+//		});
 		
 		if(ClientOptions.isCustomSky()) {
-			DimensionRenderingRegistry.registerSkyRenderer(Level.END, new BetterEndSkyRenderer());
+//			DimensionRenderingRegistry.registerSkyRenderer(Level.END, new BetterEndSkyRenderer());
 		}
+		//FIXME: Idk how to port this atm
 	}
 	
-	public static void registerTooltips() {
-		ItemTooltipCallback.EVENT.register((player, stack, context, lines) -> {
-			if (stack.getItem() instanceof CrystaliteArmor) {
-				boolean hasSet = false;
-				if (player != null) {
-					hasSet = CrystaliteArmor.hasFullSet(player);
-				}
-				TranslatableComponent setDesc = new TranslatableComponent("tooltip.armor.crystalite_set");
-				setDesc.setStyle(Style.EMPTY.applyFormats(
-					hasSet ? ChatFormatting.BLUE : ChatFormatting.DARK_GRAY,
-					ChatFormatting.ITALIC
-				));
-				lines.add(TextComponent.EMPTY);
-				lines.add(setDesc);
-			}
-		});
-	}
+//	public static void registerTooltips() {
+//		ItemTooltipCallback.EVENT.register((player, stack, context, lines) -> {
+//			if (stack.getItem() instanceof CrystaliteArmor) {
+//				boolean hasSet = false;
+//				if (player != null) {
+//					hasSet = CrystaliteArmor.hasFullSet(player);
+//				}
+//				TranslatableComponent setDesc = new TranslatableComponent("tooltip.armor.crystalite_set");
+//				setDesc.setStyle(Style.EMPTY.applyFormats(
+//					hasSet ? ChatFormatting.BLUE : ChatFormatting.DARK_GRAY,
+//					ChatFormatting.ITALIC
+//				));
+//				lines.add(TextComponent.EMPTY);
+//				lines.add(setDesc);
+//			}
+//		});
+//	}
+	//FIXME: Fix the toolTips
 }
--- src/main/java/ru/betterend/events/ItemTooltipCallback.java
+++ src/main/java/ru/betterend/events/ItemTooltipCallback.java
@@ -1,33 +1,33 @@
-package ru.betterend.events;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-
-import java.util.List;
-
-@OnlyIn(Dist.CLIENT)
-public interface ItemTooltipCallback {
-	/**
-	 * Fired after the game has appended all base tooltip lines to the list.
-	 */
-	Event<ItemTooltipCallback> EVENT = EventFactory.createArrayBacked(
-		ItemTooltipCallback.class,
-		callbacks -> (player, stack, context, lines) -> {
-			for (ItemTooltipCallback callback : callbacks) {
-				callback.getTooltip(player, stack, context, lines);
-			}
-		}
-	);
-	
-	/**
-	 * Called when an item stack's tooltip is rendered. Text added to {@code lines} will be
-	 * rendered with the tooltip.
-	 *
-	 * @param lines the list containing the lines of text displayed on the stack's tooltip
-	 */
-	void getTooltip(Player player, ItemStack stack, TooltipFlag context, List<Component> lines);
-}
+//package ru.betterend.events;
+//
+//import net.minecraftforge.api.distmarker.Dist;
+//import net.minecraftforge.api.distmarker.OnlyIn;
+//import net.minecraft.network.chat.Component;
+//import net.minecraft.world.entity.player.Player;
+//import net.minecraft.world.item.ItemStack;
+//import net.minecraft.world.item.TooltipFlag;
+//
+//import java.util.List;
+//
+//@OnlyIn(Dist.CLIENT)
+//public interface ItemTooltipCallback {
+//	/**
+//	 * Fired after the game has appended all base tooltip lines to the list.
+//	 */
+//	Event<ItemTooltipCallback> EVENT = EventFactory.createArrayBacked(
+//		ItemTooltipCallback.class,
+//		callbacks -> (player, stack, context, lines) -> {
+//			for (ItemTooltipCallback callback : callbacks) {
+//				callback.getTooltip(player, stack, context, lines);
+//			}
+//		}
+//	);
+//
+//	/**
+//	 * Called when an item stack's tooltip is rendered. Text added to {@code lines} will be
+//	 * rendered with the tooltip.
+//	 *
+//	 * @param lines the list containing the lines of text displayed on the stack's tooltip
+//	 */
+//	void getTooltip(Player player, ItemStack stack, TooltipFlag context, List<Component> lines);
+//}
